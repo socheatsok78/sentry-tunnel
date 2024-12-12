a2f929da0386bb36ab0150529e241c1478713f36
Socheat Sok <socheatsok78@gmail.com>
2024-12-12 17:11:26 +0700

    Add syntactic benchmarks

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ it:
 
 run:
 	docker run -it --rm -p 3003:3003 socheatsok78/sentry-tunnel:dev
+
+.PHONY: benchmark
+benchmark:
+	wrk -t12 -c400 -d30s -s benchmarks/envelope.lua http://localhost:3003
---
+++ benchmarks/envelope.lua
@@ -0,0 +1,6 @@
+-- example HTTP POST script which demonstrates setting the
+-- HTTP method, body, and adding a header
+
+wrk.method = "POST"
+wrk.body   = '{"sent_at":"2024-12-12T09:58:38.344Z","sdk":{"name":"sentry.javascript.vue","version":"8.26.0"},"dsn":"http://host.docker.internal:8080/0"}\n{"type":"session"}\n{"sid":"d959bbc52ffe475ca8ddf328e2a1570f","init":true,"started":"2024-12-12T09:58:38.343Z","timestamp":"2024-12-12T09:58:38.344Z","status":"ok","errors":0,"attrs":{"release":"nbcp-ncs-frontend@develop","environment":"development","user_agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"}}'
+wrk.headers["Content-Type"] = "text/plain;charset=UTF-8"
