5550c4541a1ea218095f5e0c029c6fd627e8e3d7
Socheat Sok <socheatsok78@gmail.com>
2024-12-12 16:32:43 +0700

    Improve logging

--- main.ts
+++ main.ts
@@ -7,44 +7,82 @@
 // A tunnel is an HTTP endpoint that acts as a proxy between Sentry and your application.
 // Learn more at https://docs.sentry.io/platforms/javascript/troubleshooting/#using-the-tunnel-option
 
+import * as log from "@std/log";
+
+type SentryHeaderStruct = {
+  dsn: string;
+  event_id: string;
+  sent_at: string;
+  sdk: {
+    name: string;
+    version: string;
+  };
+}
+
+
 if (import.meta.main) {
+  log.setup({
+    handlers: {
+      default: new log.ConsoleHandler("DEBUG", {
+        formatter: log.formatters.jsonFormatter,
+        useColors: false,
+      }),
+    },
+  });
+
   const options: Deno.ServeTcpOptions = {
     port: parseInt(Deno.env.get("PORT") as string || "3003"),
+    onListen(localAddr) {
+      log.info(`Listening on http://localhost:${localAddr.port}`);
+      log.info("Sentry tunnel started! Log data will stream in below:");
+    },
   }
 
   Deno.serve(options, async (req) => {
     if (req.method !== "POST") {
       return new Response(null, { status: 405 });
     }
+
     try {
       const envelopeBytes = await req.arrayBuffer();
       const envelope = new TextDecoder().decode(envelopeBytes);
       const piece = envelope.split("\n")[0];
-      const header = JSON.parse(piece);
+      const header: SentryHeaderStruct = JSON.parse(piece);
 
-      if (!header["dsn"]) {
-        console.error("No DSN found in the envelope header");
+      if (!header.dsn) {
+        log.error("No DSN found in the envelope header");
         return new Response(JSON.stringify({ error: "No DSN found in the envelope header" }), { status: 400 });
       }
 
-      const dsn = new URL(header["dsn"]);
+      // Extract the project ID from the DSN
+      const dsn = new URL(header.dsn);
       const project_id = dsn.pathname?.replace("/", "");
 
       if (!project_id) {
-        console.error("No project ID found in the DSN");
+        log.error("No project ID found in the DSN");
         return new Response(JSON.stringify({ error: "No project ID found in the DSN" }), { status: 400 });
       }
 
-      console.info("Tunneling to sentry", `${dsn}`, );
+      // Copy headers and set Host and Origin to the Sentry DSN
+      const headers = new Headers(req.headers);
+      headers.set("Host", dsn.host);
+      headers.set("Origin", dsn.origin);
 
+      // Tunnel the envelope to Sentry
       const upstream_sentry_url = `${dsn.origin}/api/${project_id}/envelope/`;
+
+      log.info(`Tunneling to sentry ${dsn}`);
+      log.info("Forwarding envelope", { sent_at: header.sent_at, event_id: header.event_id, sdk: header.sdk });
+
+      // Send the envelope to Sentry
       return fetch(upstream_sentry_url, {
         method: "POST",
+        headers: headers,
         body: envelopeBytes,
-      });
+      })
     } catch (e) {
-      console.error("Error tunneling to sentry", e);
-      return new Response(JSON.stringify({ error: "error tunneling to sentry" }), { status: 500 });
+      log.error("Error tunneling to sentry", e);
+      return new Response(JSON.stringify({ error: "Error tunneling to sentry" }), { status: 500 });
     }
   });
 }
